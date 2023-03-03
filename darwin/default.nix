30bdc907363ee4bf19e421e29ec5e0a635e67fbe
chaosinthecrd <thomas.meadows@jetstack.io>
2023-03-03 13:26:09 +0000

    changing macbook model

--- darwin/default.nix
+++ darwin/default.nix
@@ -14,7 +14,7 @@ let
   system = "aarch64-darwin";                                 # System architecture
 in
 {
-  macbook = darwin.lib.darwinSystem {                       # MacBook8,1 "Core M" 1.2 12" (2015) A1534 ECM2746 profile
+  macbook = darwin.lib.darwinSystem {                       # MacBook Pro (14-inch, 2021), M1 Pro
     inherit system;
     specialArgs = { inherit inputs; user = "tom.meadows"; };
     modules = [                                             # Modules that are used
