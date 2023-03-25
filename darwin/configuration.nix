f6311d6396df4c4e1e75f8e7ec092b6bc72667d7
chaosinthecrd <thomas.meadows@jetstack.io>
2023-03-25 15:40:52 +0000

    cool

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -63,11 +63,11 @@ in
       alacritty
 
       # Command-line tools
-      coreutils fzf ripgrep argo argocd bat colordiff cowsay toilet colima
+      coreutils fzf ripgrep argo argocd bat colordiff cowsay colima
       gawk kubectx kubectl_1_25_4 google-cloud-sdk kustomize
       helmfile kubernetes-helm htop hugo k9s krew stern crane diffoscope
 
-      minikube kind neofetch octant sipcalc tmate tree wget openssh
+      minikube kind neofetch octant sipcalc tmate tree wget openssh keychain
       watch git-crypt gnupg gpg-tui cosign jq docker-client starship diceware glow spicetify-cli
 
       # Development
@@ -101,6 +101,8 @@ in
     ];
     brews = [
       "FelixKratz/formulae/sketchybar"
+      "ddcctl"
+      "ykman"
     ];
     casks = [
       "google-chrome"
