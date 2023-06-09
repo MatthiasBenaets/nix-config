60f920f57dfd25bd90f636fe47d03ebac694f839
chaosinthecrd <tom@tmlabs.co.uk>
2023-06-09 17:56:01 +0100

    going back to original

--- users/chaosinthecrd/home-manager.nix
+++ users/chaosinthecrd/home-manager.nix
@@ -9,27 +9,17 @@ in {
   xdg.enable = true;
 
   # todo: the duplication here sucks but I can't for the life of me figure it out.
-  imports = if isDarwin
-       then [
+  imports = [
         ../../modules/shell/git.nix
         ../../modules/programs/alacritty.nix
         ../../modules/shell/zsh.nix
         ../../modules/editors/nvim/nvim.nix
         ../../pkgs/default.nix
-       ] ++
-       [
+       ] ++ (lib.optionals isDarwin [
         ../../darwin/modules/sketchybar/sketchybar.nix
         ../../darwin/modules/yabai/yabai.nix
         ../../darwin/modules/skhd/skhd.nix
-       ]
-       else [ 
-        ../../modules/shell/git.nix
-        ../../modules/programs/alacritty.nix
-        ../../modules/shell/zsh.nix
-        ../../modules/editors/nvim/nvim.nix
-        ../../pkgs/default.nix
-       ];
-
+       ]);
 
   home = {
     stateVersion = "23.05";
