e45446ca0f7a16a992e1e12da3d797653376a507
chaosinthecrd <tom@tmlabs.co.uk>
2023-06-10 14:39:10 +0100

    I think i've done it

--- machines/vm-shared.nix
+++ machines/vm-shared.nix
@@ -67,7 +67,7 @@
     };
 
     displayManager = {
-      defaultSession = "none+hyprland";
+      defaultSession = "hyprland";
       lightdm.enable = true;
 
       # AARCH64: For now, on Apple Silicon, we must manually set the
