84be742651b11d545d21d7ac705ceba6ae3425ba
chaosinthecrd <tom@tmlabs.co.uk>
2023-06-10 01:52:57 +0100

    im out of ideas here

--- machines/vm-aarch64-prl.nix
+++ machines/vm-aarch64-prl.nix
@@ -21,5 +21,5 @@
 
   # Lots of stuff that uses aarch64 that claims doesn't work, but actually works.
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnsupportedSystem = true;
+  nixpkgs.config.allowUnsupportedSystem = false;
 }
--- pkgs/kube.nix
+++ pkgs/kube.nix
@@ -4,7 +4,7 @@ with pkgs;
   home = {
     packages = with pkgs; [
       # all the kubernetes related stuff
-      argo argocd colima kubectx kubectl go_1_20 kustomize
+      argo argocd colima kubectx kubectl go_1_20 (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin]) kustomize
       helmfile kubernetes-helm k9s krew crane minikube kind octant awscli2 vault terraform cosign docker-client syft grype ko cmctl 
     ];
   };
