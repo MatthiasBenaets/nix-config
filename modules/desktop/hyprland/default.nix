22fa0a1455d572d7d18a046827870f239f2fc695
chaosinthecrd <tom@tmlabs.co.uk>
2023-06-10 13:57:59 +0100

    fixing broken else

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -19,21 +19,23 @@ in
       XDG_SESSION_TYPE="wayland";
       XDG_SESSION_DESKTOP="Hyprland";
     };
-    sessionVariables = {
-      #GBM_BACKEND = "nvidia-drm";
-      #__GL_GSYNC_ALLOWED = "0";
-      #__GL_VRR_ALLOWED = "0";
-      #WLR_DRM_NO_ATOMIC = "1";
-      #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      #_JAVA_AWT_WM_NONREPARENTING = "1";
-
-      QT_QPA_PLATFORM = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-
-      GDK_BACKEND = "wayland";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      MOZ_ENABLE_WAYLAND = "1";
-    } else {
+    // keeping for nvidia times
+    # sessionVariables = {
+    #   #GBM_BACKEND = "nvidia-drm";
+    #   #__GL_GSYNC_ALLOWED = "0";
+    #   #__GL_VRR_ALLOWED = "0";
+    #   #WLR_DRM_NO_ATOMIC = "1";
+    #   #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    #   #_JAVA_AWT_WM_NONREPARENTING = "1";
+    #
+    #   QT_QPA_PLATFORM = "wayland";
+    #   QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    #
+    #   GDK_BACKEND = "wayland";
+    #   WLR_NO_HARDWARE_CURSORS = "1";
+    #   MOZ_ENABLE_WAYLAND = "1";
+    # } else {
+      sessionVariables = {
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
 
