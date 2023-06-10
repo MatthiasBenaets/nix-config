e962d718a7fa7cd59dcfa83606fcad3f6d30a2e5
chaosinthecrd <tom@tmlabs.co.uk>
2023-06-10 02:36:05 +0100

    trying these

--- pkgs/core.nix
+++ pkgs/core.nix
@@ -6,9 +6,11 @@ with pkgs;
   home = {
     packages = with pkgs; [
       # Terminal
-      coreutils fzf ripgrep bat colordiff htop tree wget openssh diceware
-      keychain watch jq starship git gcc gnumake gawk diffoscope tmate neofetch
-      glow
+      coreutils fzf ripgrep bat htop tree wget keychain watch jq starship
+      gcc gnumake gawk
+      # coreutils fzf ripgrep bat colordiff htop tree wget openssh diceware
+      # keychain watch jq starship git gcc gnumake gawk diffoscope tmate neofetch
+      # glow
     ];
   };
   
