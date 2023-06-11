6e1803fe0919ce32c0af7e036c2a2e8cc2592c89
chaosinthecrd <tom@tmlabs.co.uk>
2023-06-11 22:45:21 +0100

    switching back to unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs =                                                                  # All flake references used to build my NixOS setup. These are dependencies.
     {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";                  # Nix Packages
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";                  # Nix Packages
       hyprland.url = "github:hyprwm/Hyprland";
 
 
