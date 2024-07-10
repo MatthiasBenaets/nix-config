bc93dd178bf5098376258f6411e1e6d60ce42257
MatthiasBenaets <matthias.benaets@gmail.com>
2024-07-10 18:24:36 +0200

    oh-my-zsh

--- darwin/modules/zsh.nix
+++ darwin/modules/zsh.nix
@@ -20,27 +20,22 @@
         enable = true;
         autosuggestion.enable = true;
         syntaxHighlighting.enable = true;
+        enableCompletion = true;
         history.size = 10000;
+        oh-my-zsh = {
+          enable = true;
+          plugins = [
+            "macos"
+          ];
+        };
         initExtra = ''
-          # Search history
-          autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
-          zle -N up-line-or-beginning-search
-          zle -N down-line-or-beginning-search
-          bindkey '^[[A' up-line-or-beginning-search
-          bindkey '^[[B' down-line-or-beginning-search
-
-          # Powerlevel10k prompt
           source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
           [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-          # Set up completion and set color for selected suggestion
-          zstyle ':completion:*' menu select
           ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#757575'
 
-          # Aliases
           alias ls="${pkgs.eza}/bin/eza --icons=always --color=always"
-          alias -g ...=../..
-          alias -g ....=../../..
+          alias finder="ofd"
         '';
       };
     };
