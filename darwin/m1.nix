b4dda615f21b4079be498313ce042017d14cf412
MatthiasBenaets <matthias.benaets@gmail.com>
2024-09-18 18:07:15 +0200

    20240918

--- darwin/m1.nix
+++ darwin/m1.nix
@@ -30,6 +30,10 @@
     masApps = {
       # "FileZilla Pro - FTP and Client" = 1298486723;
       # "FileZilla Pro RemoteDrive" = -2087754162;
+      "Keynote" = 409183694;
+      "Microsoft Remote Desktop" = 1295203466;
+      "Numbers" = 409203825;
+      "Pages" = 409201541;
       "Xcode" = 497799835;
     };
   };
