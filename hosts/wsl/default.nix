79b144780f9014c70593fe0edc1f7abe93e3973a
Reinout Holtrup <reinout91@gmail.com>
2025-12-08 22:39:03 +0100

    update vscode server

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,38 +1,15 @@
-#
-#  Specific system configuration settings for wsl
-#
-#  flake.nix
-#   ├─ ./hosts
-#   │   ├─ default.nix
-#   │   └─ ./wsl
-#   │        ├─ default.nix
-#   └─ ./modules
-#       └─ ./desktops
-#           ├─ hyprland.nix
-#           └─ ./virtualisation
-#               └─ default.nix
-#
-
 { pkgs, ... }:
 
 {
   imports = [
       (fetchTarball {
       url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
-      sha256 = "09j4kvsxw1d5dvnhbsgih0icbrxqv90nzf0b589rb5z6gnzwjnqf";
+      sha256 = "1rdn70jrg5mxmkkrpy2xk8lydmlc707sk0zb35426v1yxxka10by";
      }) ] ++
     (import ../../modules/desktops/virtualisation);
 
   wsl.enable = true;
   wsl.defaultUser = "nixos";
-
-
-  hardware = {
-    sane = {
-      enable = true;
-      extraBackends = [ pkgs.sane-airscan ];
-    };
-  };
 
   hyprland.enable = true;
 
@@ -47,12 +24,6 @@
 
   environment = {
     systemPackages = with pkgs; [
-      # ansible # Automation
-      #gmtp # Used for mounting gopro
-      # plex-media-player # Media Player
-      # simple-scan # Scanning
-      # sshpass # Ansible Dependency
-      # wacomtablet # Tablet
     ];
   };
 
