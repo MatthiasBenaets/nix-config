2441992dd9dd6df912dec14341de99f03ec0e4e7
MatthiasBenaets <matthias.benaets@gmail.com>
2026-09-14 15:34:16 +0200

    chore: sync kitty theme to nvim

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -50,13 +50,11 @@
               font_family FiraCode Nerd Font Mono
               font_size 13
 
-              background_opacity 0.8
-              background_blur 16
+              # background_opacity 0.8
+              # background_blur 16
 
               window_margin_width 4
               single_window_margin_width 0
-              active_border_color   #d0d0d0
-              inactive_border_color #202020
 
               hide_window_decorations titlebar-only
 
@@ -65,27 +63,72 @@
 
               confirm_os_window_close 0
 
-              background            #202020
-              foreground            #d0d0d0
-              cursor                #d0d0d0
-              selection_background  #303030
-              color0                #151515
-              color8                #505050
-              color1                #ac4142
-              color9                #ac4142
-              color2                #7e8d50
-              color10               #7e8d50
-              color3                #e5b566
-              color11               #e5b566
-              color4                #6c99ba
-              color12               #6c99ba
-              color5                #9e4e85
-              color13               #9e4e85
-              color6                #7dd5cf
-              color14               #7dd5cf
-              color7                #d0d0d0
-              color15               #f5f5f5
-              selection_foreground  #202020
+
+              # Tokyonight
+              foreground #a9b1d6
+              background #1a1b26
+
+              # Black
+              color0 #414868
+              color8 #414868
+
+              # Red
+              color1 #f7768e
+              color9 #f7768e
+
+              # Green
+              color2  #73daca
+              color10 #73daca
+
+              # Yellow
+              color3  #e0af68
+              color11 #e0af68
+
+              # Blue
+              color4  #7aa2f7
+              color12 #7aa2f7
+
+              # Magenta
+              color5  #bb9af7
+              color13 #bb9af7
+
+              # Cyan
+              color6  #7dcfff
+              color14 #7dcfff
+
+              # White
+              color7  #c0caf5
+              color15 #c0caf5
+
+              # Cursor
+              cursor #c0caf5
+              cursor_text_color #1a1b26
+
+              # Selection highlight
+              selection_foreground none
+              selection_background #28344a
+
+              # The color for highlighting URLs on mouse-over
+              url_color #9ece6a
+
+              # Window borders
+              active_border_color #3d59a1
+              inactive_border_color #101014
+              bell_border_color #e0af68
+
+              # Tab bar
+              tab_bar_style fade
+              tab_fade 1
+              active_tab_foreground   #3d59a1
+              active_tab_background   #16161e
+              active_tab_font_style   bold
+              inactive_tab_foreground #787c99
+              inactive_tab_background #16161e
+              inactive_tab_font_style bold
+              tab_bar_background #101014
+
+              # Title bar
+              macos_titlebar_color #16161e
 
               map ctrl+space no_op
               map cmd+f no_op
